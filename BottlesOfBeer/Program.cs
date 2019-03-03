5d9fb0f285f6fcf9ebc05e8986e70e7463422681
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-02 22:31:02 -0600

    weird bottle error when I run the test

--- BottlesOfBeer/Program.cs
+++ BottlesOfBeer/Program.cs
@@ -6,10 +6,29 @@ namespace BottlesOfBeer
     {
         public static void Main()
         {
-            // TODO: Output "99 bottles of beer on the wall.  99 bottles of beer.  Take 1 down and pass it around, 98 bottles of beer on the wall.";
-            // Count down until there are no more bottles of beer on the wall.
+            int counter = 99;
+            for (int i = 1; i < 100; i++)
+
+                counter = NewMethod(counter);
 
             Console.ReadLine();
         }
+
+        private static int NewMethod(int counter)
+        {
+            Console.WriteLine($" {counter} bottles of beer on the wall.");
+
+            Console.WriteLine($" {counter} bottles of beer");
+
+            Console.WriteLine($"Take one down. Pass it around.");
+
+            counter--;
+            
+            Console.WriteLine($" {counter} bottles of beer on the wall.");
+            Console.WriteLine();
+            return counter;
+        }
+            
+        }
     }
-}
+
